d29f7af12f7ca06c99bfc2c4fea76a7a8283d8ef
Ryan-Oliver-2k <60796472+Ryan-Oliver-2k@users.noreply.github.com>
2022-03-28 19:11:54 -0400

    Made VGA Folder

---
+++ quartus-project/VGA/VGA.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity VGA is
+    port(
+        CLOCK_25    :   in std_logic
+        
+    );
+
+end VGA
+
+architecture rtl
