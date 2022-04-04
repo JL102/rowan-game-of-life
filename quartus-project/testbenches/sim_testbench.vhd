6e1bce109235e4cc309423db9e0598010ac5e861
JL102 <jordanlees@mailbox.org>
2022-04-04 19:08:42 -0400

    Cell simulation fixes & testbench

--- quartus-project/testbenches/sim_testbench.vhd
+++ quartus-project/testbenches/sim_testbench.vhd
@@ -1,7 +1,8 @@
 library ieee;
+library work;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.package_types.all; -- see package_types.vhd
+use work.package_types.all; -- see package_types.vhd
 
 
 entity sim_testbench is -- A testbench usually has no ports
@@ -12,16 +13,30 @@ architecture tb of sim_testbench is
 	-- One signal per port is typical
 	signal reset : std_logic;
 	signal states : array_states;
-	signal start_as : array_states;
+	signal start_as : array_states := (others => (others => '0')); -- Initialize most of start_as as 0 so I can pick which ones to turn on
 	-- signal state : std_logic;
 	-- signal adjacents : std_logic_vector(7 downto 0);
 	-- signal sum : std_logic_vector(3 downto 0);
 begin
 	clk <= not clk after 5 ns;
+	
+	-- start_as(0) <= ('0', '1', '0', '0', '0', '0');
 	-- Apply stimulus and check the results
 	process
 	begin
-		wait for 40 ns;
+		reset <= '1';
+		-- Glider from playgameoflife.com
+		start_as(2, 1) <= '1';
+		start_as(3, 2) <= '1';
+		start_as(1, 3) <= '1';
+		start_as(2, 3) <= '1';
+		start_as(3, 3) <= '1';
+		
+		
+		wait for 10 ns;
+		reset <= '0';
+		
+		wait for 200 ns;
 		
 		assert false report "Test: OK" severity failure;
 	end process;
--- quartus-project/vhdl/cell_simulation.vhd
+++ quartus-project/vhdl/cell_simulation.vhd
@@ -1,7 +1,8 @@
 library ieee;
+library work;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.package_types.all; -- see package_types.vhd
+use work.package_types.all; -- see package_types.vhd
 
 -- Adjacent cells: (possibly up for modification)
 --- 7	6	5
@@ -16,8 +17,8 @@ entity cell_simulation is
 	port(
 		clk : 		in std_logic;
 		reset : 	in std_logic; -- Whether to reset to the starting state
-		start_as :  in array_states;
-		states : 	out array_states
+		start_as :  in array_states
+		; states : 	inout array_states
 --		start_as : 	in std_logic_vector(15 downto 0); -- Starting state
 --		adj : 		in std_logic_vector(7 downto 0) -- Adjacent cells' states
 --		states: 	out std_logic_vector(15 downto 0) -- Present state
@@ -33,6 +34,8 @@ architecture rtl of cell_simulation is
 			adj : 		in std_logic_vector(7 downto 0);
 			state: 		out std_logic);
 	end component;
+	
+	-- signal states : array_states;
 begin
 	
 	gen_rows :
@@ -44,103 +47,104 @@ begin
 			-- top left corner
 			gen_if1 : if row = 0 and col = 0 generate
 				gen_cell : cell port map (
-					clk, reset, 
-					start_as(row, col),
-					'0' 				 & '0' 					& '0' &
-					'0' 				 						& states(row,   col+1) &
-					'0' 				 & states(row+1, col)   & states(row+1, col+1),
-					states(row,   col)
+					clk => clk, reset => reset, 
+					start_as => start_as(row, col),
+					adj(0) => '0', adj(1) => '0',				  adj(2) => '0',
+					adj(3) => '0',								  adj(4) => states(row,   col+1),
+					adj(5) => '0', adj(6) => states(row+1, col),  adj(7) => states(row+1, col+1),
+					state => states(row,   col)
 				);
 			end generate gen_if1;
 			-- top right corner
 			gen_if2 : if row = 0 and col = MAX_COL generate
 				gen_cell : cell port map (
-					clk, reset, 
-					start_as(row, col),
-					'0' 				 & '0' 					& '0' &
-					states(row,   col-1) 						& '0' &
-					states(row+1, col-1) & states(row+1, col)   & '0',
-					states(row,   col)
+					clk => clk, reset => reset, 
+					start_as => start_as(row, col),
+					adj(0) => '0', 					adj(1) => '0', 				  adj(2) => '0',
+					adj(3) => states(row,   col-1),								  adj(4) => '0',
+					adj(5) => states(row+1, col-1), adj(6) => states(row+1, col), adj(7) => '0',
+					state => states(row,   col)
 				);
 			end generate gen_if2;
 			-- bottom left corner
 			gen_if3 : if row = MAX_ROW and col = 0 generate
 				gen_cell : cell port map (
-					clk, reset, 
-					start_as(row, col),
-					'0' 				 & states(row-1, col)   & states(row-1, col+1) &
-					'0' 				 						& states(row,   col+1) &
-					'0' 				 & '0' 					& '0' ,
-					states(row,   col)
+					clk => clk, reset => reset, 
+					start_as => start_as(row, col),
+					adj(0) => '0', adj(1) => states(row-1, col), 	adj(2) => states(row-1, col+1),
+					adj(3) => '0',								  	adj(4) => states(row,   col+1),
+					adj(5) => '0', adj(6) => '0',					adj(7) => '0',
+					state => states(row,   col)
 				);
 			end generate gen_if3;
 			-- bottom right corner
 			gen_if4 : if row = MAX_ROW and col = MAX_COL generate
 				gen_cell : cell port map (
-					clk, reset, 
-					start_as(row, col),
-					states(row-1, col-1) & states(row-1, col)   & '0' &
-					states(row,   col-1) 						& '0' &
-					'0' 				 & '0' 				 	& '0',
-					states(row,   col)
+					clk => clk, reset => reset, 
+					start_as => start_as(row, col),
+					adj(0) => states(row-1, col-1), adj(1) => states(row-1, col), adj(2) => '0',
+					adj(3) => states(row,   col-1),								  adj(4) => '0',
+					adj(5) => '0', adj(6) => '0', adj(7) => '0',
+					state => states(row,   col)
 				);
 			end generate gen_if4;
 			-- top edge
 			gen_if5 : if row = 0 and row < MAX_ROW and col > 0 and col < MAX_COL generate
 				gen_cell : cell port map (
-					clk, reset, 
-					start_as(row, col),
-					'0' 				 & '0' 					& '0' &
-					states(row,   col-1) 						& states(row,   col+1) &
-					states(row+1, col-1) & states(row+1, col)   & states(row+1, col+1),
-					states(row,   col)
+					clk => clk, reset => reset, 
+					start_as => start_as(row, col),
+					adj(0) => '0', adj(1) => '0', adj(2) => '0',
+					adj(3) => states(row,   col-1),								  adj(4) => states(row,   col+1),
+					adj(5) => states(row+1, col-1), adj(6) => states(row+1, col), adj(7) => states(row+1, col+1),
+					state => states(row,   col)
 				);
 			end generate gen_if5;
 			-- bottom edge
 			gen_if6 : if row = MAX_ROW and col > 0 and col < MAX_COL generate
 				gen_cell : cell port map (
-					clk, reset, 
-					start_as(row, col),
-					states(row-1, col-1) & states(row-1, col)   & states(row-1, col+1) &
-					states(row,   col-1) 						& states(row,   col+1) &
-					'0' 				 & '0' 					& '0' ,
-					states(row,   col)
+					clk => clk, reset => reset, 
+					start_as => start_as(row, col),
+					adj(0) => states(row-1, col-1), adj(1) => states(row-1, col), adj(2) => states(row-1, col+1),
+					adj(3) => states(row,   col-1),								  adj(4) => states(row,   col+1),
+					adj(5) => '0', 					adj(6) => '0', 				  adj(7) => '0',
+					state => states(row,   col)
 				);
 			end generate gen_if6;
 			-- left edge
 			gen_if7 : if row > 0 and row < MAX_ROW and col = 0 generate
 				gen_cell : cell port map (
-					clk, reset, 
-					start_as(row, col),
-					'0' 				 & states(row-1, col)   & states(row-1, col+1) &
-					'0' 				 						& states(row,   col+1) &
-					'0' 				 & states(row+1, col)   & states(row+1, col+1),
-					states(row,   col)
+					clk => clk, reset => reset, 
+					start_as => start_as(row, col),
+					adj(0) => '0', adj(1) => states(row-1, col), adj(2) => states(row-1, col+1),
+					adj(3) => '0',								 adj(4) => states(row,   col+1),
+					adj(5) => '0', adj(6) => states(row+1, col), adj(7) => states(row+1, col+1),
+					state => states(row,   col)
 				);
 			end generate gen_if7;
 			-- right edge
 			gen_if8 : if row > 0 and row < MAX_ROW and col = MAX_COL generate
 				gen_cell : cell port map (
-					clk, reset, 
-					start_as(row, col),
-					states(row-1, col-1) & states(row-1, col)   & '0' &
-					states(row,   col-1) 						& '0' &
-					states(row+1, col-1) & states(row+1, col)   & '0' ,
-					states(row,   col)
+					clk => clk, reset => reset, 
+					start_as => start_as(row, col),
+					adj(0) => states(row-1, col-1), adj(1) => states(row-1, col), adj(2) => '0',
+					adj(3) => states(row,   col-1),								  adj(4) => '0',
+					adj(5) => states(row+1, col-1), adj(6) => states(row+1, col), adj(7) => '0',
+					state => states(row,   col)
 				);
 			end generate gen_if8;
 			
 			-- center of the grid
 			gen_if9 : if row > 0 and row < MAX_ROW and col > 0 and col < MAX_COL generate
 				gen_cell : cell port map (
-					clk, reset, 
-					start_as(row, col),
-					states(row-1, col-1) & states(row-1, col)   & states(row-1, col+1) &
-					states(row,   col-1) 						& states(row,   col+1) &
-					states(row+1, col-1) & states(row+1, col)   & states(row+1, col+1),
-					states(row,   col)
+					clk => clk, reset => reset, 
+					start_as => start_as(row, col),
+					adj(0) => states(row-1, col-1), adj(1) => states(row-1, col), adj(2) => states(row-1, col+1),
+					adj(3) => states(row,   col-1),								  adj(4) => states(row,   col+1),
+					adj(5) => states(row+1, col-1), adj(6) => states(row+1, col), adj(7) => states(row+1, col+1),
+					state => states(row,   col)
 				);
 			end generate gen_if9;
+			-- JL note: Can't use "&" in port map because ModelSim doesn't understand it.
 		end generate;
 	end generate;
 	
