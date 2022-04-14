7e8ec928b3af0a94ddf4e1ebfdce331102e9bcad
Ryan-Oliver-2k <drunk>
2022-04-13 21:17:13 -0400

    Fixing my Fuckup

--- quartus-project/testbenches/sim_testbench.vhd
+++ quartus-project/testbenches/sim_testbench.vhd
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-library ieee;
-library work;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.package_types.all; -- see package_types.vhd
-
-
-entity sim_testbench is -- A testbench usually has no ports
-end sim_testbench;
-
-architecture tb of sim_testbench is
-	signal clk : std_logic := '0'; -- Must initialize!
-	-- One signal per port is typical
-	signal reset : std_logic;
-	signal states : array_states;
-	signal start_as : array_states := (others => (others => '0')); -- Initialize most of start_as as 0 so I can pick which ones to turn on
-	-- signal state : std_logic;
-	-- signal adjacents : std_logic_vector(7 downto 0);
-	-- signal sum : std_logic_vector(3 downto 0);
-begin
-	clk <= not clk after 5 ns;
-	
-	-- start_as(0) <= ('0', '1', '0', '0', '0', '0');
-	-- Apply stimulus and check the results
-	process
-	begin
-		reset <= '1';
-		-- Glider from playgameoflife.com
-		start_as(2, 1) <= '1';
-		start_as(3, 2) <= '1';
-		start_as(1, 3) <= '1';
-		start_as(2, 3) <= '1';
-		start_as(3, 3) <= '1';
-		
-		
-		wait for 10 ns;
-		reset <= '0';
-		
-		wait for 200 ns;
-		
-		assert false report "Test: OK" severity failure;
-	end process;
-	-- Instantiate the Unit Under Test
-	uut : entity work.cell_simulation
-	port map(
-		clk, reset, start_as, states
-		-- , sum --debugging
-	);
-end tb;
-=======
 library ieee;
 library work;
 use ieee.std_logic_1164.all;
@@ -130,4 +79,3 @@ begin
 		-- , sum --debugging
 	);
 end tb;
->>>>>>> 4cfb614c43c7adc7ab19ca1eb506debcd5152822
--- quartus-project/vhdl/work/package_types.vhd
+++ quartus-project/vhdl/work/package_types.vhd
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use STD.TEXTIO.all;
-
--- By copying the way ieee wrote their definitions for std_logic_1164, we can 
--- 	define our own types in a similar manner. (because online VHDL resources were
--- 	not helpful at this).
--- 	Simply include package_types in the project and add: use ieee.package_types.all;
-
--- C:\intelFPGA_lite\18.1\quartus\libraries\vhdl\ieee\2008
-package package_types is
-	
-	constant MAX_ROW : integer := 6;
-	constant MAX_COL : integer := 6;
-	
-	type array_states is array (MAX_ROW downto 0, MAX_COL downto 0) of std_logic;
-
-=======
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -36,5 +16,4 @@ package package_types is
 	
 	type array_states is array (MAX_ROW downto 0, MAX_COL downto 0) of std_logic;
 
->>>>>>> 4cfb614c43c7adc7ab19ca1eb506debcd5152822
 end package package_types;
