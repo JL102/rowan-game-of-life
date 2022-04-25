a0696f5501eb299d3e79c072e19600c844a5a21c
JL102 <jordanlees@mailbox.org>
2022-04-25 19:18:29 -0400

    testing from today

--- quartus-project/vhdl/cell.vhd
+++ quartus-project/vhdl/cell.vhd
@@ -14,19 +14,25 @@ end cell;
 
 architecture rtl of cell is
 	
-	component eight_bit_counter is
-		port (bits : in std_logic_vector(7 downto 0);
-			num : out std_logic_vector(3 downto 0));
-	end component;
+	-- component eight_bit_counter is
+	-- 	port (bits : in std_logic_vector(7 downto 0);
+	-- 		num : out unsigned(3 downto 0));
+	-- end component;
 	
-	signal sum : std_logic_vector (3 downto 0); -- number of bits that are '1' 000
-begin
-	
-	Counter : eight_bit_counter port map(adj, sum); -- Use the eight bit counter to identify the number of adjacent live cells
+	-- signal sum : unsigned (3 downto 0); -- number of bits that are '1' 000
+begin	
+	-- Counter : eight_bit_counter port map(adj, sum); -- Use the eight bit counter to identify the number of adjacent live cells
 	
 	process(clk)
+	variable sum : unsigned(3 downto 0) := "0000";
 	begin
 		if rising_edge(clk) then
+			sum := "0000";   --initialize count variable.
+			for i in 0 to 7 loop   --check for all the bits.
+				if(adj(i) = '1') then --check if the bit is '1'
+					sum := sum + 1; --if its one, increment the count.
+				end if;
+			end loop;
 			-- When the reset line is on, initialize the cell based on its "start_as" control line
 			if reset = '1' then
 				state <= start_as;
